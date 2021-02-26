7302d8d6a2cf748ec5324a4202dce1c044216c00
JaimeZeng <78083448+JaimeZeng@users.noreply.github.com>
2021-02-26 10:34:19 +0800

    :pencil: Update course-java2020-week0901 JavaScript入门-1.

---
+++ course-java2020/src/pub/zxj/java2020/week09/js/demo/0102.js
@@ -0,0 +1,15 @@
+var var1 = 123;
+var var2 = "123";
+var var3 = "ZhangWei";
+
+// alert 调试
+alert(var1 + 1);
+alert(var2 + 1);
+alert(var3 + 1);
+alert("引入的外部 JavaScript 文件： Hello Jaime~")
+
+// console.log 调试
+console.log(var1 + 1);
+console.log(var2 + 1);
+console.log(var3 + 1);
+console.log("引入的外部 JavaScript 文件： Hello Jaime~")
