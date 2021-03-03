332c8f45453d3e254d4c61a08c4d6b9575dfb3bb
JaimeZeng <78083448+JaimeZeng@users.noreply.github.com>
2021-03-03 10:52:33 +0800

    :pencil: Update course-java2020-week1002 Servlet 入门 - 0211 请求参数的发送与接收.

--- course-java2020/src/pub/zxj/java2020/week10/servlet/src/pub/zxj/servlet/SampleServlet.java
+++ course-java2020/src/pub/zxj/java2020/week10/servlet/src/pub/zxj/servlet/SampleServlet.java
@@ -13,9 +13,21 @@ public class SampleServlet extends HttpServlet {
     @Override
     public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
+        String method = req.getMethod();
+        String name = req.getParameter("name");
+        String mobile = req.getParameter("mobile");
+        String sex = req.getParameter("sex");
+        String[] spec = req.getParameterValues("spec");
+        StringBuilder specs = new StringBuilder();
+        for (String value : spec) {
+            specs.append(value).append(" ");
+        }
         // 向浏览器输出的数据流
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
+        out.println("<ul><li>method = " + method + "</li><li>name = " + name + "</li>" +
+                "<li>mobile = " + mobile + "</li><li>sex = " + sex + "</li>" +
+                "<li>spec = " + specs + "</li></ul>");
         out.println("<a href = 'https://www.baidu.com/'>Baidu</a>");
     }
 }
