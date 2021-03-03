1335ebf3c73f53d98c51bb5e02cead492a93865f
JaimeZeng <78083448+JaimeZeng@users.noreply.github.com>
2021-03-03 13:47:33 +0800

    :pencil: Update course-java2020-week1002 Servlet 入门 - 0205 编程练习.

---
+++ course-java2020/src/pub/zxj/java2020/week10/servlet-practice/src/pub/zxj/servlet/ShopServlet.java
@@ -0,0 +1,24 @@
+package pub.zxj.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 使用 Servlet 在页面输出商品类别名称，商品类别名称通过 url 地址进行传递，在 Servlet 中获取类别名称并输出。
+ * url地址如下所示：/ShopServlet?category=book
+ *
+ * @author Jaime
+ */
+@WebServlet(urlPatterns = "/ShopServlet")
+public class ShopServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String category = req.getParameter("category");
+        resp.setContentType("text/html");
+        // resp.getWriter().println("<p><span style = 'color: #0000ff;'>" + category + "</span></p>");
+        resp.getWriter().println("<h3 style = 'color: #0000ff;'>" + category + "</h3>");
+    }
+}
