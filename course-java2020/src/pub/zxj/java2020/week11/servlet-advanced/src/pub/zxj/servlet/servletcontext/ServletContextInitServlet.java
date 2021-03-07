d4202d8dde826a4c6550684014f4cbe7f1256cbf
JaimeZeng <78083448+JaimeZeng@users.noreply.github.com>
2021-03-07 11:12:51 +0800

    :pencil: Update course-java2020-week1004 Servlet 与 JSP 进阶 - 0502 web.xml 常用配置（下）.

--- course-java2020/src/pub/zxj/java2020/week11/servlet-advanced/src/pub/zxj/servlet/servletcontext/ServletContextInitServlet.java
+++ course-java2020/src/pub/zxj/java2020/week11/servlet-advanced/src/pub/zxj/servlet/servletcontext/ServletContextInitServlet.java
@@ -16,9 +16,16 @@ public class ServletContextInitServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         ServletContext servletContext = request.getServletContext();
-        servletContext.setAttribute("title", "Stay Simple.");
-        servletContext.setAttribute("author", "Jaime");
-        servletContext.setAttribute("copyright", "&copy; 2018-present zxj.pub 京ICP备12345678号-2");
-        response.getWriter().println("init success~");
+        // servletContext.setAttribute("title", "Stay Simple.");
+        // servletContext.setAttribute("author", "Jaime");
+        // servletContext.setAttribute("copyright", "&copy; 2018-present zxj.pub 京ICP备12345678号-2");
+        String title = servletContext.getInitParameter("title");
+        String author = servletContext.getInitParameter("author");
+        String copyright = servletContext.getInitParameter("copyright");
+        servletContext.setAttribute("title", title);
+        servletContext.setAttribute("author", author);
+        servletContext.setAttribute("copyright", copyright);
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().println("<p>init success~</p>");
     }
 }
